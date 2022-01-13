5af3d1c629571c699d4482deb6e346179898b984
evavic44 <evavic44@gmail.com>
2022-01-13 17:54:55 +0100

    Add let, var & const script

---
+++ 01-Fundamentals-Part-1/let_const_var/script.js
@@ -0,0 +1,14 @@
+// Let Const Var
+let age = 30;
+age = 31;
+
+// Const is used for declaring variables that cannot be changed
+const birthYear = 1997;
+// birthYear = 1990;
+
+// const job;
+
+var job = "programmer";
+job = "clown";
+
+console.log(job);
