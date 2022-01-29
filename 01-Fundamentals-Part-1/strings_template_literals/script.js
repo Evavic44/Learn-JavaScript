10653d2789e15dd448c5b72178d2f1606dff767f
evavic44 <evavic44@gmail.com>
2022-01-29 08:00:59 +0100

    ✨ Finish template literals

--- 01-Fundamentals-Part-1/strings_template_literals/script.js
+++ 01-Fundamentals-Part-1/strings_template_literals/script.js
@@ -1,24 +1,51 @@
-const firstName = "Eke";
-const job = "Software Engineer";
-const birthYear = 1997;
-const year = 2021;
+// const firstName = "Eke";
+// const job = "Software Engineer";
+// const birthYear = 1997;
+// const year = 2021;
 
-// ES5 string concatenation
+// // ES5 string concatenation
+// const eke =
+//   "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+// console.log(eke);
+
+// // ES6 Template Strings
+// const ekeNew = `I'm ${firstName}, a ${year - birthYear} old ${job}!`;
+// console.log(ekeNew);
+
+// // Using backticks in variable names
+// console.log(`Just a regular string ol' nation`);
+
+// // Create multi-lined string ES5
+// console.log("String with \n multiple \n lines");
+
+// // Create multi-lined string - Template literal ES6
+// console.log(`String With
+// multi
+// lines`);
+
+// Update
+const firstName = "Victor";
+const job = "Programmer";
+const birthYear = 1997;
+const year = 2022;
+
 const eke =
   "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+
 console.log(eke);
 
-// ES6 Template Strings
-const ekeNew = `I'm ${firstName}, a ${year - birthYear} old ${job}!`;
+// Template literals
+const ekeNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
 console.log(ekeNew);
 
-// Using backticks in variable names
-console.log(`Just a regular string ol' nation`);
+console.log(`This is Just a string...`);
 
-// Create multi-lined string ES5
-console.log("String with \n multiple \n lines");
+// Multi lined strings (ES5)
+console.log("String with \n\
+multiple \n\
+line");
 
-// Create multi-lined string - Template literal ES6
-console.log(`String With
-multi
+// Multi lined template string (ES6)
+console.log(`String with
+multiple
 lines`);
--- README.md
+++ README.md
@@ -690,7 +690,7 @@ Template Literals use back-ticks (``) rather than the quotes ("") to define a st
 ```js
 const firstName = "Eke";
 const job = "Software Developer";
-const birthYear = "1991";
+const birthYear = 1991;
 const year = 2021;
 ```
 
@@ -714,3 +714,42 @@ console.log(eke);
 We can see this is a pain to write and we have to manage the spacing between each string and text. This brings us to our topic **"Template Literals:"** which is an easy way of writing strings in a normal way and then insert variables directly into the string.
 
 So basically a template literal can assemble multiple pieces in one final string.
+
+**Example:**
+To write a template literal, we use a backtick to assemble the strings. In other to add the variable, we'll add the variable in a curly braces and a dollar sign before it. `${variable}`
+
+```js
+// Template literals
+const ekeNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(ekeNew);
+```
+
+You can see this is easier and shorter to write. No need for adding the plus signs to concatenate the strings.
+
+We can also use the backticks for regular expressions of strings.
+
+```js
+console.log(`This is Just a string...`);
+```
+
+Many developers adopt this method of using backticks instead of single or double quotes when defining strings, this is a choice you can make, if you want.
+
+Another good use of template literals is to create multi lined strings. Before ES6, we needed to add a `\n\` to create a new line.
+
+<!-- Multi lined strings (ES5) -->
+
+```js
+console.log(
+  "String with \n\
+multiple \n\
+lines"
+);
+```
+
+<!-- Multi lined template string (ES6) -->
+
+```js
+console.log(`String with
+multiple
+lines`);
+```
