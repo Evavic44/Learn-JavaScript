0e099fb1f7d5444eb9600df38459ed1b518a9664
Victor Eke <evavic44@gmail.com>
2022-04-25 23:58:59 +0100

    :fire: Add logical operators section

---
+++ 01-Fundamentals-Part-1/16_logical_operators/script.js
@@ -0,0 +1,21 @@
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+// if (hasDriversLicense && hasGoodVision) {
+//   console.log("Sarah is able to drive");
+// } else {
+//   console.log("Someone else should drive...");
+// }
+
+const isTired = false; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log("Sarah is able to drive");
+} else {
+  console.log("Someone else should drive...");
+}
