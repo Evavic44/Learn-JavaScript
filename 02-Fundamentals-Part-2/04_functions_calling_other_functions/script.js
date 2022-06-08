c78de94590b4aecaba9ef3952809f8ef6bd6dcb8
Victor Eke <evavic44@gmail.com>
2022-06-08 13:00:47 +0100

    :zap: functions calling other functions

---
+++ 02-Fundamentals-Part-2/04_functions_calling_other_functions/script.js
@@ -0,0 +1,13 @@
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 4));
