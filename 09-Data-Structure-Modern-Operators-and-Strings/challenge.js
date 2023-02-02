430dba1a7a32cc9a89180af720c8ae14f15e754b
Victor Eke <evavic44@gmail.com>
2023-02-02 17:16:36 +0100

    complete coding challenge

--- 09-Data-Structure-Modern-Operators-and-Strings/challenge.js
+++ 09-Data-Structure-Modern-Operators-and-Strings/challenge.js
@@ -28,6 +28,107 @@ Then, call the function again with players from game.scored
 GOOD LUCK 😀 
  **/
 
+// const game = {
+//   team1: "Bayern Munich",
+//   team2: "Borrussia Dortmund",
+//   players: [
+//     [
+//       "Neuer",
+//       "Pavard",
+//       "Martinez",
+//       "Alaba",
+//       "Davies",
+//       "Kimmich",
+//       "Goretzka",
+//       "Coman",
+//       "Muller",
+//       "Gnarby",
+//       "Lewandowski",
+//     ],
+//     [
+//       "Burki",
+//       "Schulz",
+//       "Hummels",
+//       "Akanji",
+//       "Hakimi",
+//       "Weigl",
+//       "Witsel",
+//       "Hazard",
+//       "Brandt",
+//       "Sancho",
+//       "Gotze",
+//     ],
+//   ],
+//   score: "4:0",
+//   scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+//   date: "Nov 9th, 2037",
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+// // 1.
+// const [player1, player2] = game.players;
+// console.log(player1, player2);
+
+// // 2.
+// const [gk, ...fieldPlayers] = player1;
+// console.log(gk, fieldPlayers);
+
+// // 3.
+// const allPlayers = [...player1, ...player2];
+// console.log(allPlayers);
+
+// // 4.
+// const players1Final = [...player1, "Thiago", "Coutinho", "Perisic"];
+// console.log(players1Final);
+
+// // 5.
+// // const {
+// //   odds: { team1, x: draw, team2 },
+// // } = game;
+
+// // or
+// const { team1, x: draw, team2 } = game.odds;
+// console.log(team1, draw, team2);
+
+// // 6.
+// function printGoals(...players) {
+//   console.log(...players, `${players.length} goals scored ⚽`);
+// }
+// printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+// printGoals(...game.scored);
+
+// // 7.
+// console.log(
+//   team1 < team2 && "Team 1 is more likely to win",
+//   team1 > team2 && "Team 2 is more likely to win"
+// );
+
+// ----------------------------------------
+// Coding Challenge #2
+// ----------------------------------------
+
+/* 
+Let's continue with our football betting app!
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+GOOD LUCK 😀
+*/
+
 const game = {
   team1: "Bayern Munich",
   team2: "Borrussia Dortmund",
@@ -70,39 +171,47 @@ const game = {
 };
 
 // 1.
-const [player1, player2] = game.players;
-console.log(player1, player2);
+console.log("----- 🔸Challenge 1🔸 -------");
+for (const [goal, playerName] of Object.entries(game.scored)) {
+  console.log(`Goal ${Number(goal) + 1}: ${playerName}`);
+}
+// Jonas Solution
+// for (const [i, playerName] of game.scored.entries()) {
+//   console.log(`Goal ${i + 1}: ${playerName}`);
+// }
 
 // 2.
-const [gk, ...fieldPlayers] = player1;
-console.log(gk, fieldPlayers);
+console.log("----- 🔸Challenge 2🔸 -------");
+let sum = 0;
+for (const odds of Object.values(game.odds)) {
+  sum += odds;
+}
+const average = sum / Object.values(game.odds).length;
+console.log(average);
+
+// Jonas Solution
+// const odds = Object.values(game.odds);
+// let average = 0;
+// for (const odd of odds average += odds;
+// average /= odds.length;
+// console.log(average);
 
 // 3.
-const allPlayers = [...player1, ...player2];
-console.log(allPlayers);
-
-// 4.
-const players1Final = [...player1, "Thiago", "Coutinho", "Perisic"];
-console.log(players1Final);
-
-// 5.
-// const {
-//   odds: { team1, x: draw, team2 },
-// } = game;
-
-// or
-const { team1, x: draw, team2 } = game.odds;
-console.log(team1, draw, team2);
-
-// 6.
-function printGoals(...players) {
-  console.log(...players, `${players.length} goals scored ⚽`);
-}
-printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
-printGoals(...game.scored);
-
-// 7.
+console.log("----- 🔸Challenge 3🔸 -------");
 console.log(
-  team1 < team2 && "Team 1 is more likely to win",
-  team1 > team2 && "Team 2 is more likely to win"
+  `Odd of victory ${game.team1}: ${game.odds.team1} 🟢
+Odd of draw: ${game.odds.x} 🟡
+Odd of victory ${game.team2}: ${game.odds.team2} 🔴
+  `
 );
+
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === "x" ? "draw" : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
+}
+
+const scorers = {};
+for (const player of game.scored) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+console.log(scorers);
